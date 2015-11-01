3eda803d8d5d2fb217512a7700d2ebde1485f538
Nil Thacker <nil@nil.gs>
2015-10-31 20:18:57 -0700

    Fix css spacing issue

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,5 +1,5 @@
 function popUp(URL) {
   day = new Date();
   id = day.getTime();
-  eval("page"+id+" = window.open(URL, '"+id+"', 'toolbar=0, scrollbars=1, location=0, statusbar=0, menubar=0, resizable=1, width=690, height=920, left = 1030, top = 460');");
+  eval("page"+id+" = window.open(URL, '"+id+"', 'toolbar=0, scrollbars=1, location=0, statusbar=0, menubar=0, resizable=1, width=690, height=920');");
 }
